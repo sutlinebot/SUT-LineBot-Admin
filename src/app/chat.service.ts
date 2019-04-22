1a632b15d99782eee2a696b511d4a0f835e75e73
anantakit <40273343+anantakit@users.noreply.github.com>
2019-04-22 19:53:56 +0700

    close #20 chat

---
+++ src/app/chat.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { environment } from '../environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+export class Message {
+  constructor(public content: string, public sentBy: string) {}
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ChatService {
+
+  // readonly token = environment.dialogflow.angularBot;
+  // readonly client = new ApiAiClient({ accessToken: this.token });
+
+  conversation = new BehaviorSubject<Message[]>([]);
+
+  constructor(private http: HttpClient) { }
+
+  // Sends and receives messages via DialogFlow
+  converse(msg: string) {
+    const token = document.cookie.split('=')[2];
+    const httpHeaders = new HttpHeaders({'Content-Type': 'application/json', Authorization: 'Bearer ' + token});
+    const data = {
+      queryInput: {
+        text: {
+          languageCode: 'th',
+          text: msg
+        }
+      }
+    };
+    const userMessage = new Message(msg, 'user');
+    this.update(userMessage);
+
+    return this.http.post('https://dialogflow.googleapis.com/v2/projects/botframe-2d07e/agent/sessions/my_session_id:detectIntent'
+    , data, {headers: httpHeaders});
+  }
+  // Adds message to source
+  update(msg: Message) {
+    this.conversation.next([msg]);
+  }
+
+}
