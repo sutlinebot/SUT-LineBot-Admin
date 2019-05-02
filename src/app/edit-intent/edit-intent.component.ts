27a1c51ac70ef7c0e771fa7ccf4bfdde654f9d5d
Miwtoo <miwyugioh@gmail.com>
2019-05-03 03:08:42 +0700

    close #32 - can edit

--- src/app/edit-intent/edit-intent.component.ts
+++ src/app/edit-intent/edit-intent.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { DialogflowService } from '../shared/api/dialogflow.service';
+import { HttpClient } from '@angular/common/http';
+import { Router, NavigationExtras } from '@angular/router';
 
 @Component({
   selector: 'app-edit-intent',
@@ -8,13 +10,36 @@ import { Router } from '@angular/router';
 })
 export class EditIntentComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  intents;
+
+  constructor( private myapi: DialogflowService, private http: HttpClient,private router: Router) { }
 
   ngOnInit() {
     if (this.isLogin()) {
       this.router.navigate(['/login']);
     }
+    this.getintentList();
+  }
 
+  getintentList() {
+    this.myapi.getIntentList().subscribe((data: any) => {
+      this.intents = data.intents;
+      this.intents
+        .map((cur, index, arr) => {
+          this.intents[index] = [cur.displayName, cur.name]
+        });
+    });
+  }
+
+  editIntent(data){
+    //console.log(data);
+
+    let navigationExtras: NavigationExtras = {
+      queryParams: {
+          "data": JSON.stringify(data)
+      }
+    };
+    this.router.navigate(['/edit-single'],navigationExtras);
   }
 
   isLogin(){
@@ -24,5 +49,6 @@ export class EditIntentComponent implements OnInit {
     }
     return true;
   }
+  
 
 }
